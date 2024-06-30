fb245d46806758c101204ba2e988e5186c5101b3
tommytz <ttardizuch@gmail.com>
2024-06-30 22:02:11 +1200

    Added nullable extensions class

---
+++ Entities/NullableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MangaReader.Entities
+{
+    public static class NullableExtensions
+    {
+        public static T ThrowIfNull<T>(
+            this T? argument,
+            string? message = default,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = default
+        ) where T : notnull
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return argument;
+        }
+
+        public static T ThrowIfNull<T>(
+            this T? argument,
+            string? message = default,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = default
+        ) where T : struct
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return (T)argument;
+        }
+    }
+}
